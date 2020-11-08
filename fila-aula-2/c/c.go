df3baadc7d2352c497c6b33f2ada0559d942d55e
Rebeca <rebeca.fonseca@gko.com.br>
2020-11-08 10:47:52 -0300

    fila aula-2

---
+++ fila-aula-2/c/c.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+type Coupon struct {
+	Code string
+}
+
+type Coupons struct {
+	Coupon []Coupon
+}
+
+func (c Coupons) Check(code string) string {
+	for _, item := range c.Coupon {
+		if code == item.Code {
+			return "valid"
+		}
+	}
+	result := makeHttpCall("http://localhost:9093", code)
+
+	return result.Status
+}
+
+type Result struct {
+	Status string
+}
+
+var coupons Coupons
+
+func main() {
+	coupon := Coupon{
+		Code: "abc",
+	}
+
+	coupons.Coupon = append(coupons.Coupon, coupon)
+
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9092", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+	valid := coupons.Check(coupon)
+
+	result := Result{Status: valid}
+
+	jsonResult, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error converting json")
+	}
+
+	fmt.Fprintf(w, string(jsonResult))
+
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) Result {
+
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+
+	res, err := retryClient.PostForm(urlMicroservice, values)
+	if err != nil {
+		result := Result{Status: "Servidor fora do ar!"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result
+
+}
